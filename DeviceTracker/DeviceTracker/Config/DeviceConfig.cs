f995e65355dbe77d9d884b1903455c375564b049
Patrick Meier <104093287+MKPatrick@users.noreply.github.com>
2023-10-23 19:23:58 +0200

    ScanForDevice is not working properly

--- DeviceTracker/DeviceTracker/Config/DeviceConfig.cs
+++ DeviceTracker/DeviceTracker/Config/DeviceConfig.cs
@@ -3,6 +3,6 @@
     public class DeviceConfig
     {
         public const string CONFIGKEY = "DeviceConfig";
-        public Device Devices { get; set; }
+        public Device[] Devices { get; set; }
     }
 }
--- DeviceTracker/DeviceTracker/Program.cs
+++ DeviceTracker/DeviceTracker/Program.cs
@@ -1,12 +1,13 @@
 ﻿
+using DeviceTracker;
 using DeviceTracker.Config;
 
+const int SCANTIME = 5;
 var cfg = new ConfigurationManager();
-
-
+BLEScanner bleScanner = new(cfg.ResolveConfig<DeviceConfig>(DeviceConfig.CONFIGKEY));
+Console.WriteLine("Starting..");
 var resolvedCFG = cfg.ResolveConfig<MQTTConfig>(MQTTConfig.CONFIGKEY);
 
-Console.WriteLine(resolvedCFG.BrokerHost);
 
+  var result=await   bleScanner.ScanForDevices(SCANTIME);
 
-Console.ReadLine();
